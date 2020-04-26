9d81fbbf7cedf17e2e88fb51b8b29967a0159d39
NEONKID <clax1412@outlook.com>
2020-04-27 01:01:18 +0900

    Add querydsl...

--- src/main/java/com/midas2018mobile5/serverapp/Model/External/Account/AccountAuth.java
+++
@@ -1,9 +0,0 @@
-package com.midas2018mobile5.serverapp.Model.External.Account;
-
-import lombok.Data;
-
-@Data
-public class AccountAuth {
-    public String userid;
-    public String password;
-}
--- src/main/java/com/midas2018mobile5/serverapp/Model/External/Order/OrderDto.java
+++
@@ -1,8 +0,0 @@
-package com.midas2018mobile5.serverapp.Model.External.Order;
-
-import lombok.Data;
-
-@Data
-public class OrderDto {
-    public String userid;
-}
